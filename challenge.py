aee0ef44515582206bcd09797546f147ed5f11c0
Paamadoud <diengpa@ept.sn>
2025-05-04 11:03:18 +0200

    V1.0.8: Update challenge.py

--- challenge.py
+++ challenge.py
@@ -8,10 +8,10 @@ from sklearn.metrics import classification_report
 from gensim.models import Word2Vec
 import json
 import numpy as np
-version_ = "V.1.0.7"
+version_ = "V.1.0.8"
 print("Version:", version_)
 print("Loading training data...")
-with open('train_mini.json', 'r', encoding='utf-8') as f:
+with open('train.json', 'r', encoding='utf-8') as f:
     train_data = json.load(f)
 df = pd.DataFrame(train_data)
 print("Checking for problematic rows in 'description'...")
@@ -94,7 +94,7 @@ clf_w2v.fit(X, y)
 
 
 print("Loading test data...")
-with open('test_mini.json', 'r', encoding='utf-8') as f:
+with open('test.json', 'r', encoding='utf-8') as f:
     test_data = json.load(f)
 test_df = pd.DataFrame(test_data)
 
